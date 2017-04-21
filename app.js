8abb1604d19de67b38261394eeaf68fec3bbaa64
Alice Guan <aliguan726@gmail.com>
2017-04-21 00:18:48 -0400

    Add random joke view

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const Chuck = require('chucknorris-io');
 
 const app = express();
 const client = new Chuck();
 
-// ------------------------------------
+app.use(express.static('public'));
+app.use(expressLayouts);
 
+app.set('layout', 'layouts/main-layout.ejs');
+app.set('views','views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+    res.render('home-view.ejs');
+});
 
 app.get('/random', (req, res, next) => {
 
   client.getRandomJoke().then((jokeData) => {
-    console.log('Here is the joke info');
-    console.log(jokeData);
 
     res.render('random-joke-view.ejs', {
-      joke: ___
+        joke: jokeData.value,
     });
   });
 
 });
+
+app.listen('3000');
